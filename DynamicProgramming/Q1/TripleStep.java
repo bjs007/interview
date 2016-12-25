03d845c4f27812a7e4030ef9a9f3dfc92e607f81
Bijay Sharma <bijays.nitdgp@gmail.com>
2016-12-24 21:27:42 -0600

    Files updated

--- DynamicProgramming/Q1/TripleStep.java
+++ DynamicProgramming/Q1/TripleStep.java
@@ -8,10 +8,27 @@ public static  int step(int n){
     return step(n-1) + step(n-2) + step(n-3);
   }
 
+  public static  int stepMemo(int n,int memo[]){
+      if(n<0)
+      return 0;
+      else if(n==0)
+      return 1;
+      else if(memo[n] > 0)
+      return memo[n];
+      else {
+        memo[n] = stepMemo(n-1,memo) + stepMemo(n-2,memo) + stepMemo(n-3,memo);
+        return memo[n];
+      }
+
+    }
+
   public static void main(String args[]){
       int n = 20;
-      int count = step(n);
-      System.out.println("Number of ways"+count);
+      int count1 = step(n);
+      int memo[] = new int[n+1];
+      int count2 = stepMemo(n,memo);
+
+      System.out.println("Number of ways"+count1);
   }
 
 }
